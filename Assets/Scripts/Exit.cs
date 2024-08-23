9b4965230a40b4d9af302a64e569fd2a2d485293
Kendra Gamett <kgamett@student.neumont.edu>
2024-08-22 21:15:52 -0600

    Win Screen Shows Score

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
     [SerializeField] GameObject winScreen;
+    [SerializeField] TMP_Text pointsText;
+    [SerializeField] Player player;
     [SerializeField] float displayWinScreenTimer = 3;
 
     bool exited = false;
@@ -30,6 +33,7 @@ public class Exit : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player") winScreen.SetActive(true);
+        pointsText.text =  "You got " + player.collectables.ToString() + " points!";
         exited = true;
     }
 }
