2c213dcf7d2d67e39a912fd69244397f43d2662e
Kendra Gamett <kgamett@student.neumont.edu>
2024-08-22 20:57:23 -0600

    Title Screen

--- Assets/Scripts/Exit.cs
+++ Assets/Scripts/Exit.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Exit : MonoBehaviour
 {
     [SerializeField] GameObject winScreen;
+    [SerializeField] float displayWinScreenTimer = 3;
+
+    bool exited = false;
 
     void Start()
     {
@@ -13,11 +17,19 @@ public class Exit : MonoBehaviour
 
     void Update()
     {
-        
+        if(exited)
+        {
+            displayWinScreenTimer -= Time.deltaTime;
+            if(displayWinScreenTimer < 0 )
+            {
+                SceneManager.LoadSceneAsync("Title", LoadSceneMode.Single);
+            }
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player") winScreen.SetActive(true);
+        exited = true;
     }
 }
